e1f5aa38e6ec111d9bb117c6f9f8c95de1e17365
declipz <declipz1121@gmail.com>
2018-11-15 14:55:58 +0300

    Changed .gitignore

---
+++ Weather/Core/Networking/OpenWeatherService.swift
@@ -0,0 +1,9 @@
+//
+//  OpenWeatherService.swift
+//  Weather
+//
+//  Created by Vladislav Kondrashkov on 11/15/18.
+//  Copyright © 2018 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
